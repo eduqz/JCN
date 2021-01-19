0746d8231f07b3854415bc3e92d623fa703bd046
EsterPrazim <eap4@cin.ufpe.br>
2021-01-18 21:12:56 -0300

    Add model depositions

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Depositions
 
 # Register your models here.
+class DepositionsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'work')
+
+admin.site.register(Depositions, DepositionsAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Depositions(models.Model):
+    name = models.CharField('Nome da pessoa', max_length=50)
+    work = models.CharField('Empresa ou ocupação', max_length=50)
+    deposition = models.TextField('Texto', null=True, blank=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Depoimento'
+        verbose_name_plural = 'Depoimentos'
+
+    def __str__(self):
+        return self.name 
