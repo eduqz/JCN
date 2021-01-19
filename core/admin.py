32c0cb154e737b8b38ce053f63db37719fd9762e
EsterPrazim <eap4@cin.ufpe.br>
2021-01-18 21:21:45 -0300

    Add model banner

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Depositions
+from .models import Depositions, Banner
+from solo.admin import SingletonModelAdmin
 
 # Register your models here.
 class DepositionsAdmin(admin.ModelAdmin):
     list_display = ('name', 'work')
 
 admin.site.register(Depositions, DepositionsAdmin)
+admin.site.register(Banner, SingletonModelAdmin)
--- core/models.py
+++ core/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from solo.models import SingletonModel
 
 # Create your models here.
+class Banner(SingletonModel):
+    title = models.CharField('Título', max_length=60)
+    
+    class Meta:
+        verbose_name = 'Banner'
+
+    def __str__(self):
+        return "Banner"
+
 class Depositions(models.Model):
     name = models.CharField('Nome da pessoa', max_length=50)
     work = models.CharField('Empresa ou ocupação', max_length=50)
