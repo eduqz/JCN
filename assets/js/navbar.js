7ab5fe2f84891980827312aaf3ef7d674cd674b5
EsterPrazim <eap4@cin.ufpe.br>
2021-02-04 12:19:28 -0300

    navbar web e mobile

---
+++ assets/js/navbar.js
@@ -0,0 +1,11 @@
+var $doc = $('html, body');
+$('.jsNavbar').click(function() {
+    document.getElementById('navbar-mobile').classList.toggle('active');
+    document.getElementById('navbar-menu').classList.toggle('deactive');
+});
+
+var $doc = $('html, body');
+$('.jsNavbarClose').click(function() {
+    document.getElementById('navbar-mobile').classList.toggle('active');
+    document.getElementById('navbar-menu').classList.toggle('deactive');
+});
