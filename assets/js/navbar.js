24118acdf6333299269249112ae07f240f93ccad
EsterPrazim <eap4@cin.ufpe.br>
2021-02-05 12:36:36 -0300

    fix scroll navbar

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -9,3 +9,11 @@ $('.jsNavbarClose').click(function() {
     document.getElementById('navbar-mobile').classList.toggle('active');
     document.getElementById('navbar-menu').classList.toggle('deactive');
 });
+
+var $doc = $('html, body');
+$('.scrollSuave').click(function() {
+    $doc.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+});
