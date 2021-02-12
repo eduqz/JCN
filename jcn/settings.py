5a641738190fb73b539f9d47be1ccd3602c7bead
Ester Prazim <eap4@cin.ufpe.br>
2021-02-12 00:01:50 -0300

    config ssl

--- jcn/settings.py
+++ jcn/settings.py
@@ -153,5 +153,9 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 import django_heroku
 django_heroku.settings(locals())
 
+SECURE_SSL_REDIRECT = True
+
+SECURE_PROXY_SSL_HEADER = (‘HTTP_X_FORWARDED_PROTO’, ‘https’) 
+
 DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
 DROPBOX_OAUTH2_TOKEN = config('ACCESS_TOKEN_DROPBOX') 
