abf9a7a8105f85197e03e79b424c16be3f5f47e8
Ester Prazim <eap4@cin.ufpe.br>
2021-02-12 00:09:19 -0300

    Fix 'https'

--- jcn/settings.py
+++ jcn/settings.py
@@ -155,7 +155,4 @@ django_heroku.settings(locals())
 
 SECURE_SSL_REDIRECT = True
 
-SECURE_PROXY_SSL_HEADER = (‘HTTP_X_FORWARDED_PROTO’, ‘https’) 
-
-DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
-DROPBOX_OAUTH2_TOKEN = config('ACCESS_TOKEN_DROPBOX') 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
